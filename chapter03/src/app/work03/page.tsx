450e3c0841d14f91250efda2622da328a11aab54
Hiroshi Fujiwara <fujiwara@no1s.biz>
2024-11-20 05:30:31 +0000

    added chapter03

---
+++ chapter03/src/app/work03/page.tsx
@@ -0,0 +1,107 @@
+'use client';
+
+import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+
+// スキーマ定義
+const schema = z.object({
+  name: z.string().min(1, { message: '名前は必須です' }),
+  email: z.string().email({ message: 'メールは必須です' }),
+  age: z.number({ message: '年齢は数値です' }),
+});
+
+// スキーマ定義から型を推論
+type InputsType = z.infer<typeof schema>;
+
+export default function Page() {
+  // ReactHookFormとは本質的には関係ない
+  const [data, setData] = useState({});
+  const [open, setOpen] = useState(false);
+  // ここまで
+
+  // フォームの初期化
+  // register -> 入力要素にハンドラーを登録する関数
+  // handleSubmit -> サブミット時に実行するイベントハンドラ関数
+  // errors -> 入力要素のエラー
+  // InputType -> 入力フ要素の型
+  // resolver -> zod連携
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InputsType>({ resolver: zodResolver(schema) });
+
+  // サブミット時に実行する関数
+  const onSubmit: SubmitHandler<InputsType> = (data) => {
+    // ここでは、state変数に代入している
+    // 実際にはサーバーに送信などを行う
+    setData(data);
+    setOpen(true);
+  };
+
+  return (
+    <div className="main">
+      <h1 className="p-4 bg-blue-200 text-xl">Work03</h1>
+      <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 text-start">
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 m-4 col-start-1 md:col-start-2 lg:col-start-2 col-span-1 md:col-span-3 lg:col-span-1">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-4">
+              <label className="block mb-2" htmlFor="name">
+                Name
+              </label>
+              <input
+                className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:shadow-outline focus:border-blue-500"
+                {...register('name')}
+              />
+              <div className="mt-1 mb-4 text-red-500 font-bold">
+                {errors.name?.message}
+              </div>
+            </div>
+            <div className="mb-4">
+              <label className="block mb-2" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:shadow-outline focus:border-blue-500"
+                {...register('email')}
+              />
+              <div className="mt-1 mb-4 text-red-500 font-bold">
+                {errors.email?.message}
+              </div>
+            </div>
+            <div className="mb-4">
+              <label className="block mb-2" htmlFor="age">
+                Age
+              </label>
+              <input
+                className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:shadow-outline focus:border-blue-500"
+                {...register('age', { valueAsNumber: true })}
+              />
+              <div className="mt-1 mb-4 text-red-500 font-bold">
+                {errors.age?.message}
+              </div>
+            </div>
+            <div className="mb-4">
+              <button
+                type="submit"
+                className="w-full p-3 rounded bg-blue-500 text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+              >
+                登録
+              </button>
+            </div>
+          </form>
+          <hr className="my-8 border-gray-500" />
+          {open && (
+            <div className="mt-8">
+              <div className="mb-4">Name: {data.name}</div>
+              <div className="mb-4">Email: {data.email}</div>
+              <div className="mb-4">Age: {data.age}</div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
